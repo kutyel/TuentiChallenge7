2a509d7d2076883c3dd3e8a3a00ed1f78a89df4d
kutyel <flaviocorpa@gmail.com>
2017-04-28 15:00:09 +0200

    Subset Sum approach to Challenge 3

--- Challenge 3/script.js
+++ Challenge 3/script.js
@@ -1,16 +1,26 @@
 'use strict'
 
 const fs = require('fs')
-const input = './testInput.txt'
+const input = './test.txt'
 const output = './output.txt'
 
 fs.unlink(output, err => err && console.error(err))
 
 let c = 0
 
-function findSmallestSubset (n) {
-  const results = [1, 2, 3, 4, 5]
-  return results[Math.floor(Math.random() * 4) + 1]
+const sum = x => x.reduce((y, z) => y + z, 0)
+const range = x => Array.from(Array(x).keys()).map(y => y + 1)
+
+const subsetSum = (numbers, target, partial = []) => {
+  const s = sum(partial)
+
+  if (s === target) {
+    console.log(`${partial.join('+')}=${target}`)
+  }
+
+  if (s >= target) { return }
+
+  numbers.forEach((n, i) => subsetSum(numbers.slice(i + 1), target, partial.concat([n])))
 }
 
 fs.readFileSync(input).toString().split('\n').forEach((num, t) => {
@@ -18,8 +28,7 @@ fs.readFileSync(input).toString().split('\n').forEach((num, t) => {
     c = num
   } else if (t <= c) {
     const p = +num
-    const r = findSmallestSubset(p)
-
+    const r = subsetSum(range(p), p)
     const result = `Case #${t}: ${r}`
     console.log(result)
     fs.appendFileSync(output, `${result}\n`)

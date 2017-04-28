62f97b852799c067c1281661efb0f11c2954f951
kutyel <flaviocorpa@gmail.com>
2017-04-28 23:19:05 +0200

    Another approach to 3

--- Challenge 3/script.js
+++ Challenge 3/script.js
@@ -1,34 +1,22 @@
 'use strict'
 
 const fs = require('fs')
-const input = './test.txt'
+const input = './testInput.txt'
 const output = './output.txt'
+const { sqrt, round } = Math
 
 fs.unlink(output, err => err && console.error(err))
 
 let c = 0
 
-const sum = x => x.reduce((y, z) => y + z, 0)
-const range = x => Array.from(Array(x).keys()).map(y => y + 1)
-
-const subsetSum = (numbers, target, partial = []) => {
-  const s = sum(partial)
-
-  if (s === target) {
-    console.log(`${partial.join('+')}=${target}`)
-  }
-
-  if (s >= target) { return }
-
-  numbers.forEach((n, i) => subsetSum(numbers.slice(i + 1), target, partial.concat([n])))
-}
+const subsetSum = n => round(sqrt(2 * n + 1 / 4) - 1 / 2)
 
 fs.readFileSync(input).toString().split('\n').forEach((num, t) => {
   if (t === 0) {
     c = num
   } else if (t <= c) {
     const p = +num
-    const r = subsetSum(range(p), p)
+    const r = subsetSum(p)
     const result = `Case #${t}: ${r}`
     console.log(result)
     fs.appendFileSync(output, `${result}\n`)

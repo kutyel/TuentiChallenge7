9a8af5e6e30cb1c26777d5f22b5461d794a3442b
kutyel <flaviocorpa@gmail.com>
2017-04-29 08:28:25 +0200

    Solve Challenge 3! :tada:

--- Challenge 3/script.js
+++ Challenge 3/script.js
@@ -1,22 +1,20 @@
 'use strict'
 
 const fs = require('fs')
-const input = './testInput.txt'
+const input = './submitInput.txt'
 const output = './output.txt'
-const { sqrt, round } = Math
+const { log2, ceil } = Math
 
 fs.unlink(output, err => err && console.error(err))
 
 let c = 0
-
-const subsetSum = n => round(sqrt(2 * n + 1 / 4) - 1 / 2)
 
 fs.readFileSync(input).toString().split('\n').forEach((num, t) => {
   if (t === 0) {
     c = num
   } else if (t <= c) {
     const p = +num
-    const r = subsetSum(p)
+    const r = ceil(log2(p))
     const result = `Case #${t}: ${r}`
     console.log(result)
     fs.appendFileSync(output, `${result}\n`)
